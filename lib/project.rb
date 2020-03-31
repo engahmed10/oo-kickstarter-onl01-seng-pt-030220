090906f9de5bc45055f7129ce1cdcd6a5f1109e1
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-31 07:09:42 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+require "pry"
+class Project
+  attr_reader :title
+  def initialize(title)
+    @title=title
+    @backers =[]
+  end
+  def backers
+    @backers
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    if ! backer.backed_projects.include?(self)
+      backer.back_project(self)
+    end
+  end
+end
