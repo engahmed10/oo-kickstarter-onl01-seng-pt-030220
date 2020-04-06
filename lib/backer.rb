3156351a6873e5b14ccf820939c2add57cea4255
AHMED AFANDY <eng.computer1@yahoo.com>
2020-04-06 01:10:38 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,15 @@
 class Backer
-     attr_reader :name
-    def initialize(name)
+
+attr_reader :name,:backed_projects
+
+   def initialize(name)
       @name=name
       @backed_projects =[]
-    end
+   end
 
-    def backed_projects
-       @backed_projects
-    end
-    def back_project(project)
-        @backed_projects <<  project
-        project.add_backer(self)
-        #binding.pry
-    end
+  def back_project(project)        #project is instance of project ,we will 1-added to backed_projects array
+                                   #2-we will add our backer to project
+     @backed_projects  << project
+     project.add_backer(self)
+  end
 end
--- lib/project.rb
+++ lib/project.rb
@@ -1,18 +1,17 @@
-require "pry"
 class Project
-  attr_reader :title
-  def initialize(title)
-    @title=title
-    @backers =[]
-  end
-  def backers
-    @backers
-  end
 
-  def add_backer(backer)
-    @backers << backer
-    if ! backer.backed_projects.include?(self)
-      backer.back_project(self)
-    end
-  end
+   attr_reader :title, :backers
+   
+   def initialize(title)
+     @title=title
+     @backers=[]
+   end
+
+   def add_backer(backer)
+       @backers << backer
+       if ! backer.backed_projects.include?self
+         backer.back_project(self)
+       end
+   end
+
 end
